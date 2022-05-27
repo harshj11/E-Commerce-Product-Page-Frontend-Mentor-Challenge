2eda0bbdc042dd684ec19bc3947204e1de86ba52
Harsh Jain <harshjain5454@gmail.com>
2022-05-27 23:07:45 +0530

    :sparkles:CREATE repsonsive header - without cart card

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const hamburger = document.getElementById("hamburger");
+const sidemenu = document.getElementById("nav__sidemenu");
+const closeHamburger = document.getElementById("close__button");
+const overlay = document.getElementById("overlay");
+
+const toggleSidemenu = () => {
+    sidemenu.classList.toggle('active');
+    sidemenu.classList.toggle('hide');
+    overlay.classList.toggle('active');
+    overlay.classList.toggle('hide');
+}
+
+hamburger.addEventListener('click', toggleSidemenu);
+closeHamburger.addEventListener('click', toggleSidemenu);
+overlay.addEventListener('click', toggleSidemenu);
